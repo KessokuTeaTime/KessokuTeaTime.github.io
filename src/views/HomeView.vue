83b11ab070d7f70f95ac24c8a140aae9806a497f
KrLite <68179735+KrLite@users.noreply.github.com>
2024-08-03 17:40:27 +0800

    Workable fonts!

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
     <h1 class="band-color">This is an Welcome Page</h1>
+    <h1 class="band-color font-mplus-2p">結束</h1>
+    <h1 class="band-color font-naishomoji">バンド</h1>
   </div>
 </template>
