9c9908855031b49b70a537f3105079484155457b
KrLite <68179735+KrLite@users.noreply.github.com>
2024-08-03 18:44:39 +0800

    404 view

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,5 +1,21 @@
 <template>
   <main>
-    <h1>Page Not Found</h1>
+    <h1 class="font-mplus-2p">
+      <p>Service</p>
+      <p>Only Available in</p>
+      <p>Snimokitazawa, Japan</p>
+      <p class="primary font-space-grotesk">&gt;&gt; 404</p>
+    </h1>
   </main>
 </template>
+
+<style scoped>
+h1 {
+  color: var(--color-border);
+}
+
+.primary {
+  color: var(--color-text);
+  font-weight: bolder;
+}
+</style>
