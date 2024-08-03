aa6a91a38cd9f0869e9d04523082165bf37f83ed
KrLite <68179735+KrLite@users.noreply.github.com>
2024-08-03 13:35:24 +0800

    Basic navigations + sass

---
+++ src/components/navigation/NavBar.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <main>
+    <nav>
+      <div class="leading"></div>
+      <div class="body">
+        <RouterLink to="/">Home</RouterLink>
+      </div>
+      <div class="trailing"></div>
+    </nav>
+  </main>
+</template>
+
+<style scoped>
+nav {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 5rem;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  z-index: 1;
+
+  > * {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-weight: bold;
+  }
+}
+
+.leading,
+.body,
+.trailing {
+  border: 1px solid red;
+}
+</style>
