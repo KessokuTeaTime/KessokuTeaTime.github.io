a50efc4db2855051ca23b9dcc6b1d5aa86433d3f
KrLite <68179735+KrLite@users.noreply.github.com>
2024-08-03 19:44:28 +0800

    Fancy main page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,9 @@
 <template>
-  <div class="about">
-    <h1 class="tint">
-      <span class="font-mplus-2p">結束</span>
-      &nbsp;
-      <span class="font-naishomoji">バンド</span>
-    </h1>
-  </div>
+  <h1 v-for="i in ['kita', 'ryo', 'bocchi', 'nijika']" :class="`tint-${i}`" :key="i">
+    <span class="font-mplus-2p">結束</span>
+    &nbsp;
+    <span class="font-naishomoji">ティータイム</span>
+  </h1>
 </template>
 
 <style scoped>
